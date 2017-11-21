30221955f39ab09c8cf5abca82d42d3ce31615e5
Leobouloc <leo.bou@gmail.com>
2017-11-21 19:02:49 +0100

    cleaning

--- merge_machine/__init__.py
+++ merge_machine/__init__.py
@@ -5,4 +5,9 @@ Created on Wed Nov  8 13:46:03 2017
 
 @author: m75380
 """
+from sys import version_info
 
+class NotSupportedException(BaseException): pass
+
+if version_info.major < 3:
+    raise NotSupportedException("Only Python 3.x Supported")  
--- setup.py
+++ setup.py
@@ -8,14 +8,6 @@ Created on Tue Nov  7 13:16:56 2017
 
 from distutils.core import setup
 
-from sys import version_info
-
-class NotSupportedException(BaseException): pass
-
-if version_info.major < 3:
-    raise NotSupportedException("Only Python 3.x Supported")  
-
-
 setup(
   name = 'merge_machine',
   packages = ['merge_machine'], # this must be the same as the name above
@@ -23,7 +15,7 @@ setup(
   description = 'A library for extreme fuzzy tabular data matching that relies on Elasticsearch',
   author = 'EIG 2017',
   author_email = 'leo.bou@gmail.com',
-  # url = 'https://github.com/XXXXXXX', # use the URL to the github repo
+  url = 'https://github.com/eig-2017/Merge-Machine', # use the URL to the github repo
   # download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
   # keywords = ['testing', 'logging', 'example'], # arbitrary keywords
   classifiers = [],
