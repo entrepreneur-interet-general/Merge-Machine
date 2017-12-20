2fb4bae26097b357d2849e90015210f989945c35
Leobouloc <leo.bou@gmail.com>
2017-12-20 14:58:54 +0100

    started working on multiple queries

--- merge_machine/es_match.py
+++ merge_machine/es_match.py
@@ -27,6 +27,54 @@ import pandas as pd
 from .helpers import _bulk_search
 
 
+
+def _is_match(resp, threshold):
+    return bool(resp['hits']['hits']) and (resp['hits']['max_score'] >= threshold)
+
+def _has_hits(resp):
+    return bool(resp['hits']['hits'])
+
+def _first_match(list_of_responses, list_of_thresholds):
+    '''
+    Return the first real match (above threshold) and if not return the first 
+    non empty result if possible. 
+    
+    INPUT: 
+        list_of_responses: 
+    '''
+    assert len(list_of_responses) == len(list_of_thresholds)
+    
+    for i, resp in enumerate(list_of_responses):
+        if _has_hits(resp) and _is_match(resp):
+            return resp
+    for i, resp in enumerate(list_of_responses):
+        if _has_hits(resp):
+            return resp
+    return 0, list_of_responses[0]
+            
+
+def _bulk_search_to_full_response(res_of_bulk_search, list_of_thresholds):
+    '''
+    Takes the output _bulk_search
+    
+    
+    INPUT:
+        full_responses: flat list of results for search for rows x query_templates
+            Ex: [res_row1_qA, res_row1_qB, res_row2_qA, res_row2_qB, ...]
+        list_of_thresholds: Thresholds associated to each query
+    
+    OUTPUT:
+        new_full_responses: list with the same number of items as original rows
+            with each element being the first match found
+    '''
+    
+    num_queries_per_row = len(list_of_thresholds)
+    assert len(res_of_bulk_search) % num_queries_per_row == 0
+    
+    res_of_bulk_search = [res_of_bulk_search[i] for i in range(len(res_of_bulk_search))] # Don't use items to preserve order
+    return [_first_match(res_of_bulk_search[n:n+num_queries_per_row], list_of_thresholds) \
+                      for n in range(0, len(res_of_bulk_search), num_queries_per_row)]
+
 def es_linker(es, source, params):
     '''
     Link source to reference following instructions in params. Return 
@@ -45,10 +93,10 @@ def es_linker(es, source, params):
     '''
     
     index_name = params['index_name']
-    query_template = params['query_template']
+    query_templates = params['query_templates']
     must_filters = params.get('must', {})
     must_not_filters = params.get('must_not', {})
-    threshold = params['thresh']
+    thresholds = params['thresholds']
     exact_pairs = params.get('exact_pairs', [])
     non_matching_pairs = params.get('non_matching_pairs', [])
     
@@ -56,38 +104,35 @@ def es_linker(es, source, params):
     exact_ref_indices = [x[1] for x in exact_pairs if x[1] is not None if x[0] in source.index]
     source_indices = [x[0] for x in source.iterrows() if x [0] not in exact_source_indices]
     
-    def _is_match(f_r, threshold):
-        return bool(f_r['hits']['hits']) and (f_r['hits']['max_score'] >= threshold)
-    
-    def _has_match(f_r):
-        return bool(f_r['hits']['hits'])
+
     
     # Perform matching on non-exact pairs (not labelled)
     if source_indices:
         rows = (x[1] for x in source.iterrows() if x[0] in source_indices)
-        all_search_templates, full_responses = _bulk_search(es, index_name, [query_template], rows, must_filters, must_not_filters, num_results=1)
-        full_responses = [full_responses[i] for i in range(len(full_responses))] # Don't use items to preserve order
+        all_search_templates, res_of_bulk_search = _bulk_search(es, index_name, query_templates, rows, must_filters, must_not_filters, num_results=1)
+        full_responses = _bulk_search_to_full_response(res_of_bulk_search, thresholds)
+        del res_of_bulk_search
 
         # TODO: remove threshold condition
-        matches_in_ref = pd.DataFrame([f_r['hits']['hits'][0]['_source'] \
-                                   if _has_match(f_r) \
+        matches_in_ref = pd.DataFrame([resp['hits']['hits'][0]['_source'] \
+                                   if _has_hits(resp) \
                                    else {} \
-                                   for f_r in full_responses], index=source_indices)
+                                   for resp in full_responses], index=source_indices)
                         
-        ref_id = pd.Series([f_r['hits']['hits'][0]['_id'] \
-                                if _has_match(f_r) \
+        ref_id = pd.Series([resp['hits']['hits'][0]['_id'] \
+                                if _has_hits(resp) \
                                 else np.nan \
-                                for f_r in full_responses], index=matches_in_ref.index)
+                                for resp in full_responses], index=matches_in_ref.index)
     
-        confidence = pd.Series([f_r['hits']['hits'][0]['_score'] \
-                                if _has_match(f_r) \
+        confidence = pd.Series([resp['hits']['hits'][0]['_score'] \
+                                if _has_hits(resp) \
                                 else np.nan \
-                                for f_r in full_responses], index=matches_in_ref.index)
+                                for resp in full_responses], index=matches_in_ref.index)
         
-        confidence_gap = pd.Series([f_r['hits']['hits'][0]['_score'] - f_r['hits']['hits'][1]['_score']
-                                if (len(f_r['hits']['hits']) >= 2) and _has_match(f_r) \
+        confidence_gap = pd.Series([resp['hits']['hits'][0]['_score'] - resp['hits']['hits'][1]['_score']
+                                if (len(resp['hits']['hits']) >= 2) and _has_hits(resp) \
                                 else np.nan \
-                                for f_r in full_responses], index=matches_in_ref.index)
+                                for resp in full_responses], index=matches_in_ref.index)
 
         matches_in_ref.columns = [x + '__REF' for x in matches_in_ref.columns]
         matches_in_ref['__IS_MATCH'] = confidence >= threshold
@@ -107,7 +152,7 @@ def es_linker(es, source, params):
     # Perform matching exact (labelled) pairs
     if exact_ref_indices:
         full_responses = [es.get(index_name, ref_idx) for ref_idx in exact_ref_indices]
-        exact_matches_in_ref = pd.DataFrame([f_r['_source'] for f_r in full_responses], 
+        exact_matches_in_ref = pd.DataFrame([resp['_source'] for resp in full_responses], 
                                             index=exact_source_indices)
         exact_matches_in_ref.columns = [x + '__REF' for x in exact_matches_in_ref.columns]
         exact_matches_in_ref['__ID_REF'] = exact_ref_indices
