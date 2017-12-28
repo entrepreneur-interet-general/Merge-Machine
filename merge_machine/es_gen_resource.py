3a6c62392c792b76aa1779fe1fd2e245df6c9bb0
lleeoo <leo@bouloc.eu>
2017-12-28 20:10:29 +0100

    updated city and country analyzers and adapted resource generation

--- merge_machine/es_gen_resource.py
+++ merge_machine/es_gen_resource.py
@@ -14,7 +14,6 @@ https://data.opendatasoft.com/explore/dataset/geonames-all-cities-with-a-populat
 
 Valid match if no token on one of both sides
 
-
 """
 from collections import defaultdict
 import json
@@ -22,6 +21,8 @@ import os
 import requests
 import urllib
 import urllib.request
+
+from unidecode import unidecode
 
 curdir = os.path.dirname(os.path.realpath(__file__))
 os.chdir(curdir)
@@ -32,7 +33,8 @@ elasticsearch_resource_dir = '/etc/elasticsearch'
 # Helper functions
 #==============================================================================
 
-def write_keep_syn(name_alt_gen, file_path_keep, file_path_syn):
+def write_keep_syn(name_alt_gen, file_path_keep, file_path_syn,
+                   asciifolding=True, chars_to_replace=['-', "'"]):
     '''Write both a filter (keep) and synonym file to use as Elasticsearch
     resources.
     
@@ -57,14 +59,23 @@ def write_keep_syn(name_alt_gen, file_path_keep, file_path_syn):
     with open(file_path_syn, 'w') as w_syn, \
          open(file_path_keep, 'w') as w_keep:
         for name, alternates in name_alt_gen:
-            alternates = [y for y in [x.strip() for x in alternates] if y]
+            alternates = set(alternates)
+            alternates.add(name)
+            if asciifolding:
+                alternates = [unidecode(x) for x in alternates]
+            for char in chars_to_replace:
+                alternates = [x.replace(char, ' ') for x in alternates]
+                
+            alternates = {x.strip(', ') for x in alternates}
+            alternates = {x.replace(' ', '_') for x in alternates}
+            alternates = {x for x in alternates if x}
+            name = name.replace(' ', '_')
+            
             # sea biscuit, sea biscit => seabiscuit
-            if name in alternates:
-                alternates = set(alternates)
-                alternates.remove(name)
             if alternates:
                 string = ', '.join(alternates) + ' => ' + name + '\n'
                 w_syn.write(string)
+            
             w_keep.write(name + '\n')
             for alternate in alternates:
                 w_keep.write(alternate + '\n')
@@ -82,19 +93,20 @@ def gen_resource_city():
     if not os.path.isfile(file_path):
         if not os.path.isdir(os.path.split(file_path)[0]):
             os.makedirs(os.path.split(file_path)[0])
+        # TODO: change for CSV, take less space...
         # View data here https://data.opendatasoft.com/explore/dataset/geonames-all-cities-with-a-population-1000%40public/export
-        url = 'https://data.opendatasoft.com/explore/dataset/geonames-all-cities-with-a-population-1000@public/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true'
+        url = 'https://data.opendatasoft.com/explore/dataset/geonames-all-cities-with-a-population-1000@public/download/?format=json&timezone=Europe/Berlin&use_labels_for_header=true'
         print('Downloading resource (100M) from:\n{0}\nWriting to:\n{1}\nThis may take some time...'.format(url, file_path))
         urllib.request.urlretrieve(url, file_path)
         
     with open(file_path) as f:
         res = json.load(f)
     
-    no_country_count = 0
+    # no_country_count = 0
     no_alternate_count = 0
     
-    countries = set()
-    cities_to_countries = defaultdict(set)
+    # countries = set()
+    # cities_to_countries = defaultdict(set)
     name_to_alternates = defaultdict(set)
     for i, row in enumerate(res):
         if i%10000 == 0:
@@ -109,25 +121,25 @@ def gen_resource_city():
             alternates = []
             no_alternate_count += 1
             
-        if 'country' in my_row:
-            
-            if alternates:
-                name_to_alternates[name].update(alternates)
-            
-            country = my_row['country']
-    
-            countries.add(country)
-            for city in [name] + alternates:
-                cities_to_countries[city].add(country)
-        else:
-            no_country_count += 1
+        if alternates:
+            name_to_alternates[name].update(alternates)
+        
+        
+        #        if 'country' in my_row:
+        #            country = my_row['country']
+        #    
+        #            countries.add(country)
+        #            for city in [name] + alternates:
+        #                cities_to_countries[city].add(country)
+        #        else:
+        #            no_country_count += 1
     
     name_alt_gen = name_to_alternates.items()
     
     
     # Paths to synonym and keep files for the city analyzer 
-    file_path_keep = os.path.join(elasticsearch_resource_dir, 'es_city_keep.txt')
-    file_path_syn = os.path.join(elasticsearch_resource_dir, 'es_city_synonyms.txt')
+    file_path_keep = os.path.join(elasticsearch_resource_dir, 'city_keep.txt')
+    file_path_syn = os.path.join(elasticsearch_resource_dir, 'city_synonyms.txt')
     
     # Create the resource files (needs sudo rights)
     write_keep_syn(name_alt_gen, file_path_keep, file_path_syn)
@@ -191,8 +203,9 @@ def gen_resource_country():
     
     name_alt = []
     for country in countries:
-        name = country['name']['common']
-        alt = []
+        name = country['cca3']
+        
+        alt = [country['name']['common']]
         
         # Add native names to alternates
         if country['name']['native']:
@@ -207,8 +220,9 @@ def gen_resource_country():
         # Add altSpellings, and country codes to alternates
         alt.extend(country['altSpellings'])
         alt.extend(country[key] for key in ['cca2', 'cca3', 'ccn3'])
-            
+    
         alt = list(set(alt))
+        
         name_alt.append((name, alt))
     
     write_keep_syn(name_alt, file_path_keep, file_path_syn)
@@ -216,5 +230,5 @@ def gen_resource_country():
 if __name__ == '__main__':
 #    gen_resource_city()
 #    gen_resource_organization()
-    
+    gen_resource_city()
     gen_resource_country()
