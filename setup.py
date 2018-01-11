f88ea7b62ee06419424d901929c010ec7c38bb82
Leobouloc <leo.bou@gmail.com>
2018-01-11 20:08:15 +0100

    version bump

--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ from distutils.core import setup
 setup(
   name = 'merge_machine',
   packages = ['merge_machine'], # this must be the same as the name above
-  version = '0.1.3',
+  version = '0.1.4',
   description = 'A library for extreme fuzzy tabular data matching that relies on Elasticsearch',
   author = 'EIG 2017',
   author_email = 'leo.bou@gmail.com',
   url = 'https://github.com/eig-2017/Merge-Machine', # use the URL to the github repo
-  download_url = 'https://github.com/eig-2017/Merge-Machine/archive/0.1.3.tar.gz',
+  download_url = 'https://github.com/eig-2017/Merge-Machine/archive/0.1.4.tar.gz',
   keywords = ['csv', 'link', 'merge', 'elasticsearch'],
   classifiers = [],
   python_requires='>=3',
